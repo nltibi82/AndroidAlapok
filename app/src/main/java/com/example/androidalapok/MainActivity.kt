3087ba347280ec6057b29c34419f93110279947c
Nádas Tibor László <nltibi82@gmail.com>
2026-09-14 13:56:49 +0200

    Compose: kattintásszámláló state-tel

--- app/src/main/java/com/example/androidalapok/MainActivity.kt
+++ app/src/main/java/com/example/androidalapok/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidalapok.ui.theme.AndroidAlapokTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +34,7 @@ class MainActivity : ComponentActivity() {
             AndroidAlapokTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Tibi",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,16 +45,56 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    var clicks = remember { mutableStateOf(0) }
+    Column(
         modifier = modifier
-    )
+            .fillMaxHeight()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Text(
+            text = "Szia $name!"
+        )
+        Spacer(
+            modifier = Modifier
+                .weight(1f)
+        )
+        Text(
+            text = "Android tanulás!",
+        )
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(
+                text = "Android!"
+            )
+            Text (
+                text="Kotlin"
+            )
+        }
+        Text (
+            text = "Kattintások: ${clicks.value}"
+        )
+        Button(
+            onClick = {
+                clicks.value++
+            }
+        ) {
+            Text(
+                text = "Kattints!",
+            )
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidAlapokTheme {
-        Greeting("Android")
+        Greeting("Tibi")
     }
 }
