b7f0726f84285fa09745f156bb0c7f851c1bdec3
Nádas Tibor <nltibi82@gmail.com>
2026-09-17 18:14:36 +0200

    Compose: Card teljes szélességűvé tétele

--- app/src/main/java/com/example/androidalapok/Greeting.kt
+++ app/src/main/java/com/example/androidalapok/Greeting.kt
@@ -1,13 +1,10 @@
 package com.example.androidalapok
-
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,7 +14,7 @@ import androidx.compose.ui.unit.dp
 fun Greeting(
     name: String,
     modifier: Modifier = Modifier,
-    clicks: Int
+    clicks: Int,
 )
 {
     Column(
@@ -33,8 +30,8 @@ fun Greeting(
         )
         Row(
             modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
+                .padding(16.dp),
+            //.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
@@ -44,9 +41,12 @@ fun Greeting(
                 text = "Kotlin"
             )
         }
-
-        StatusMessage(
-            clicks = clicks
-        )
+        Card (
+            modifier = Modifier.fillMaxWidth()
+        ){
+            StatusMessage(
+                clicks = clicks
+            )
+        }
     }
 }
--- app/src/main/java/com/example/androidalapok/MainActivity.kt
+++ app/src/main/java/com/example/androidalapok/MainActivity.kt
@@ -4,28 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.androidalapok.ui.theme.AndroidAlapokTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     Greeting(
                         name = "Tibi",
                         modifier = Modifier.padding(innerPadding),
-                        clicks = clicks
+                        clicks = clicks,
                     )
                     Counter(
                         onClick = { value ->
@@ -61,6 +50,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     AndroidAlapokTheme {
-        Greeting("Tibi", clicks = 0)
+        Greeting("Tibi", clicks = 0,)
     }
 }
